6d4c1edc0a26daff3fc891b98a6db29e15a3ad34
PEI-SHAN HSIEH <SONIA@PEI-SHANs-MacBook-Pro.local>
2014-04-15 20:30:57 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Datetrack::Application.config.secret_key_base = '8d690c1bdaf02007233291a5642264efe57856822666fd497bd276ad606fe1a4902d3055d3085e827b6255027e600d922a80dd42912a38a50e519f665d0eef7b'
