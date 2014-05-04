dc366a9bc48a74fe38e6b8fe4864beebd81c9047
PEI-SHAN HSIEH <SONIA@PEI-SHANs-MacBook-Pro.local>
2014-05-04 15:13:38 -0400

    minor layout changed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if @user
       flash[:notice] = "You've been logged in."
       session[:user_id] = @user.id
-      redirect_to "/"
+      redirect_to "/experiences"
     else
       flash[:alert] = "There was a problem logging you in."
        redirect_to log_in_path
