33dd6daee530275ebd493b1077f224d8e4f93b2e
Johnwolfe <jwman1.4142@gmail.com>
2014-05-13 23:04:32 -0400

    fixes no user in databse error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  	@current_user ||= User.where(session[:user_id]).first if session[:user_id]
   end
   
   helper_method :current_user
